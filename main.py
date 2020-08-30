14d55e0507dea8c6690d488d1b2c00064668d70d
Ritesh <riteshsarkar192001@gmail.com>
2020-08-30 16:37:07 +0530

    update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("What is your name")
-print("Hello world")#ask about your world experiences
-print()
+grocery=("Harpic","vim bar","deodrant","Bhindi","Lollypop",56)print(grocery[1])
+numbers=[3,8,0,7,8]
+numbers.sort()
+numbers.reverse()
+print(numbers[1:4])
