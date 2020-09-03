a7f23631738dcaff5d8bde90071b5dd6c95b94e8
Ritesh <riteshsarkar192001@gmail.com>
2020-09-03 08:05:17 +0530

    update

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-grocery=("Harpic","vim bar","deodrant","Bhindi","Lollypop",56)
-print(grocery[1])
-numbers=[3,8,0,7,8]
-numbers.sort()
-numbers.reverse()
-print(numbers[1:4])
+str=("RITESH SARKAR")
+x=str.casefold()
+print(x)
